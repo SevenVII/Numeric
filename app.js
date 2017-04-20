355f58ca0a48dd44436b51b03d99be00c2de1225
Seven <guillermo.jose.garcia.gonzalez@gmail.com>
2017-04-20 22:15:11 +0200

    Basic parameters app
    Default configuration of a basic app

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-var express = require('express');
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
 var app = express();
 
-const pathToIndex="C:/Users/Seven/Desktop/Numeric/";
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static("core"));
+// development only
+if ('development'==app.get('env')) {
+  app.use(express.errorHandler());
+}
 
-app.get('/core/', function (req, res) {
-  res.sendFile(pathToIndex);
-  console.log("hihi: " + app.path());
-});
+app.get('/', routes.index);
+app.get('/users', user.list);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
