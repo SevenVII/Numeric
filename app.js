03a32c6735763e73c1b03270ac609cbb8a3b09c6
Seven <guillermo.jose.garcia.gonzalez@gmail.com>
2017-04-23 20:18:13 +0200

    Reecoded with a clean light app

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 1337;
-const ai = require("./AI/ai.js");
+//const ai = require("./AI/ai.js");
+const $ = require('cheerio');
+const decoded = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"];
+const encoded = ["▲","╘","╓","╕","►","╜","╒","╖","▼","╛","╙","╚","╔","╗","◄","╝","║","╩","╠","╦","■","╣","═","╪","╫","╬","─","└","┌","┐","┘","│","┴","├","┬","┤"];
 
 app.use(express.static("public"));
 
 app.get('/public/', function (req, res) {
+  var x,y;
+  var coords;
+  var alfanum;
   res.sendFile("index.html");
+  
+  x=Math.floor((Math.random()*17));
+  y=Math.floor((Math.random()*17));
+  
+  coords=x*y;
+  alfanum=$("#c" + coords).text();
+  console.log("alfanum: " + alfanum);
 });
 
 app.listen(port, function () {
