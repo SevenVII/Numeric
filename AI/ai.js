25686daa2ee5599b5407bf4ab2adc73f58f69fe9
Seven <guillermo.jose.garcia.gonzalez@gmail.com>
2017-04-23 21:59:55 +0200

    Adding new functions in ai.js in order to modify result view page

--- AI/ai.js
+++ AI/ai.js
@@ -1,1 +1,27 @@
 
+module.exports = {
+  foo: function () {
+    console.log("foo");
+  },
+  bar: function () {
+    console.log("bar");
+  }
+};
+
+var mainFunction = function(){
+	const $ = require('cheerio');
+	const decoded = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"];
+	const encoded = ["▲","╘","╓","╕","►","╜","╒","╖","▼","╛","╙","╚","╔","╗","◄","╝","║","╩","╠","╦","■","╣","═","╪","╫","╬","─","└","┌","┐","┘","│","┴","├","┬","┤"];
+	var x,y;
+	var coords;
+	var alfanum;
+
+	console.log("haha");
+
+	x=Math.floor((Math.random()*17));
+	y=Math.floor((Math.random()*17));
+
+	coords=x*y;
+	alfanum=$("#c" + coords).text();
+	console.log("alfanum: " + alfanum);
+};
--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 1337;
-//const ai = require("./AI/ai.js");
-const $ = require('cheerio');
-const decoded = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"];
-const encoded = ["▲","╘","╓","╕","►","╜","╒","╖","▼","╛","╙","╚","╔","╗","◄","╝","║","╩","╠","╦","■","╣","═","╪","╫","╬","─","└","┌","┐","┘","│","┴","├","┬","┤"];
+var AIFunctions = require("./AI/ai.js");
 
 app.use(express.static("public"));
 
 app.get('/public/', function (req, res) {
-  var x,y;
-  var coords;
-  var alfanum;
-  res.sendFile("index.html");
-  
-  x=Math.floor((Math.random()*17));
-  y=Math.floor((Math.random()*17));
-  
-  coords=x*y;
-  alfanum=$("#c" + coords).text();
-  console.log("alfanum: " + alfanum);
+	res.sendFile("index.html");
 });
 
 app.listen(port, function () {
-  console.log('Numeric app listening on port ' + port + '!');
+	console.log('Numeric app listening on port ' + port + '!');
 });
+
+console.log(typeof AIFunctions.foo);
+console.log(typeof AIFunctions.bar);
+console.log(typeof AIFunctions.mainFunction);
