feb653150e065bfa394dbe6ecda8e03c72be5e57
Seven <guillermo.jose.garcia.gonzalez@gmail.com>
2017-04-24 09:04:46 +0200

    Using cheerio get html response on the fly

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 1337;
-var AIFunctions = require("./AI/ai.js");
+const AIFunctions = require("./AI/ai.js");
 
 app.use(express.static("public"));
-
+console.log("1");
 app.get('/public/', function (req, res) {
+	console.log("2");
 	res.sendFile("index.html");
+	console.log("3");
 });
+console.log("4");
 
 app.listen(port, function () {
 	console.log('Numeric app listening on port ' + port + '!');
 });
+console.log("5");
 
-console.log(typeof AIFunctions.foo);
-console.log(typeof AIFunctions.bar);
-console.log(typeof AIFunctions.mainFunction);
+AIFunctions.mainFunction();
+
+app.get()
